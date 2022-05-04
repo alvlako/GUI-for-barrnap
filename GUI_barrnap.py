ec29074b543e8fa81e8ec9dbd45a6bf0cda1be06
MVFofanov <mvfofanov@mail.ru>
2022-05-04 19:58:47 +0700

    added ability to save results

--- GUI_barrnap.py
+++ GUI_barrnap.py
@@ -1,5 +1,4 @@
 import PySimpleGUI as sg
-import os
 import subprocess
 
 ROW_SIZE = (15, 1)
@@ -9,14 +8,8 @@ def add_option(option, default=None, size=ROW_SIZE):
     return [sg.Text(option, size=size), sg.InputText(default, key=f'-{option.upper()}-')]
 
 
-def add_input_file(file_path='file.fasta', size=ROW_SIZE):
-    return [sg.Text('Input file path', size=size), sg.InputText(file_path, key='-INPUT_FILE-'), sg.FileBrowse()]
-
-
-def remove_pref(string):
-    if string.startswith('--'):
-        return string[len('--'):]
-    return string
+def add_file(text, file_path=None, key=None, size=ROW_SIZE):
+    return [sg.Text(text, size=size), sg.InputText(file_path, key=key), sg.FileBrowse()]
 
 
 def main():
@@ -24,7 +17,7 @@ def main():
 
     options_dic = {'Kingdom': 'bac', 'Threads': '1', 'Lencutoff': '0.8', 'Reject': '0.25', 'Evalue': '1e-06'}
 
-    layout = [[sg.Text('Welcome to barrnap')], add_input_file()]
+    layout = [[sg.Text('Welcome to barrnap')], add_file('Input file path', 'file.fasta', '-INPUT_FILE-')]
     layout.extend([add_option(*option) for option in options_dic.items()])
     layout.extend([[sg.Text('Typed command', size=ROW_SIZE), sg.InputText(key='-OUTPUT-')], [sg.Submit(), sg.Cancel()]])
 
@@ -43,24 +36,18 @@ def main():
             command = ' '.join(command)
 
             window['-OUTPUT-'].update(command)
-            print(command)
+            #print(command)
 
     window.close()
-    #del window
     
-    
-    comm = []
-    for arg in arg_list:
-        comm.append(remove_pref(arg))
-    print(comm)
-
+    comm = [arg.replace('--', '') for arg in arg_list]
+    #print(comm)
     stream = subprocess.Popen(comm, stdout=subprocess.PIPE, encoding='utf-8')
     out = stream.stdout.read()
 
     layout = [[(sg.Text('Barrnap output', size=[40, 1]))],
-    [sg.Output(size=(80, 20))],
-    [sg.Multiline(size=(70, 5), enter_submits=True),
-    sg.Button('SEE', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
+    [sg.Multiline(out, size=(80, 20))],
+    [sg.Text('Output file path', size=(20,1)), sg.InputText('result.gff', key='-OUTPUT_FILE-'), sg.FileBrowse(),
     sg.Button('SAVE', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
     sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
 
@@ -68,13 +55,15 @@ def main():
 
 
     while True:
-        event, value = window.read()
-        if event == 'SEE':
-            print(out)
-        #if event == ''
+        event, values = window.read()
+        if event == 'SAVE':
+            with open(f"{values['-OUTPUT_FILE-']}", 'w') as outfile:
+                outfile.write(out)
         else:
             break
+
     window.close()
+    del window
 
     return command
 
