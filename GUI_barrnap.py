544ad24852034155b984331a14134e23409f9789
alvlako <56854264+alvlako@users.noreply.github.com>
2022-05-04 11:20:11 +0200

    add passing command to barrnap and output window

--- GUI_barrnap.py
+++ GUI_barrnap.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+import os
+import subprocess
 
 ROW_SIZE = (15, 1)
 
@@ -9,6 +11,12 @@ def add_option(option, default=None, size=ROW_SIZE):
 
 def add_input_file(file_path='file.fasta', size=ROW_SIZE):
     return [sg.Text('Input file path', size=size), sg.InputText(file_path, key='-INPUT_FILE-'), sg.FileBrowse()]
+
+
+def remove_pref(string):
+    if string.startswith('--'):
+        return string[len('--'):]
+    return string
 
 
 def main():
@@ -28,16 +36,46 @@ def main():
             break
         if event == 'Submit':
             command = ['barrnap']
+            command.append(f"{values['-INPUT_FILE-']}")
             for option in options_dic:
                 command.append(f"--{option.lower()} {values[f'-{option.upper()}-']}")
-            command.append(f"{values['-INPUT_FILE-']}")
+            arg_list = command
             command = ' '.join(command)
 
             window['-OUTPUT-'].update(command)
             print(command)
 
     window.close()
-    del window
+    #del window
+    
+    
+    comm = []
+    for arg in arg_list:
+        comm.append(remove_pref(arg))
+    print(comm)
+
+    stream = subprocess.Popen(comm, stdout=subprocess.PIPE, encoding='utf-8')
+    out = stream.stdout.read()
+
+    layout = [[(sg.Text('Barrnap output', size=[40, 1]))],
+    [sg.Output(size=(80, 20))],
+    [sg.Multiline(size=(70, 5), enter_submits=True),
+    sg.Button('SEE', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
+    sg.Button('SAVE', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
+    sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
+
+    window = sg.Window('GUI barrnap application', layout, default_element_size=(30, 2))
+
+
+    while True:
+        event, value = window.read()
+        if event == 'SEE':
+            print(out)
+        #if event == ''
+        else:
+            break
+    window.close()
+
     return command
 
 
